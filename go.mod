cea79327531edef8e428921d961d8ba81a786004
Hiroya Hinomori <nexearth@gmail.com>
2020-07-17 21:11:58 +0900

    wip

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/google/uuid v1.1.1
+	github.com/jinzhu/gorm v1.9.14
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.7 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"mock-api/handler"
 )
 
 func main() {
+	db := connectDB()
+	defer db.Close()
+
 	e := echo.New()
 
 	e.Use(middleware.Logger())
@@ -20,4 +25,19 @@ func main() {
 	r.POST("", handler.User)
 
 	e.Logger.Fatal(e.Start(":3200"))
+}
+
+func connectDB() *gorm.DB {
+	dbms := "mysql"
+	user := "root"
+	password := "password"
+	protocol := "tcp(localhost:3306)"
+	dbname := "Dummy_scheme"
+
+	connect := user+":"+password+"@"+protocol+"/"+dbname
+	db, error := gorm.Open(dbms, connect)
+	if error != nil {
+		panic(error.Error())
+	}
+	return db
 }
